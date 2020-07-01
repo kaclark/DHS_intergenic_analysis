743d19a1bb0b9941af89b26027b509ecaf11f3e4
Keanu Clark <35940194+da1andonly68@users.noreply.github.com>
2020-07-01 15:38:44 -0500

    Update nearest_gene.py

--- nearest_gene.py
+++ nearest_gene.py
@@ -1,10 +1,10 @@
 #OUTDATED
 
 #The current version of this file uses full genes and not constructed genes
-#Finds nearest gene in TADs that DHS intergenic site has membership in
+#Finds nearest gene in domains that DHS intergenic site has membership in
 
-#TADs_with_full_genes_#.csv generated from gene_analsysis.py
-#DHS_with_TADs.csv generated in TAD_analsysis.py
+#domains_with_full_genes_#.csv generated from gene_analsysis.py
+#DHS_with_domains.csv generated in domain_analsysis.py
 
 #Exports DHS_#_nearest_full_gene_distance.csv
 
@@ -12,34 +12,32 @@ import pandas as pd
 import math
 import time
 printed = False
-# files = ['1', '2', '4', '8']
-#It is faster to seperate this into multiple jobs on luria cluster instead of running one by one
-files = ['4']
+files = ['1', '2', '4', '8']
 last_time = 0
 for file in files:
-    TADs_with_genes = {}
-    DHSs_with_TADs = {}
+    domains_with_genes = {}
+    DHSs_with_domains = {}
     DHS_and_distance = []
-    #path = "data/DHS_" + file + "_cell_with_TAD.csv"
-    #TAD data doesn't have uniform columns
-    path = "data/mm10_data/TADs_with_full_genes_" + file + ".csv"
-    with open(path) as TAD_genes:
+    #path = "data/DHS_" + file + "_cell_with_domain.csv"
+    #domain data doesn't have uniform columns
+    path = "data/mm10_data/domains/domains_with_genes_" + file + ".csv"
+    with open(path) as domain_genes:
         #split file into lines
-        lis = [line.split() for line in TAD_genes] 
+        lis = [line.split() for line in domain_genes] 
         for bulk in lis:
-            TAD = None
-            #Scope of the genes from the tad data
-            #TAD data and gene data is formatted: chr#:start-end
+            domain = None
+            #Scope of the genes from the domain data
+            #domain data and gene data is formatted: chr#:start-end
             #These features are being loaded from that format
-            for TAD_data in bulk:
+            for domain_data in bulk:
                 gene_list = []
                 genes = []
-                TAD_data = TAD_data.split(',')
-                #First element is TAD id
-                TAD = TAD_data[0]
+                domain_data = domain_data.split(',')
+                #First element is domain id
+                domain = domain_data[0]
                 #All remaining elements are genes or empty space
-                TAD_removed = TAD_data[1:]
-                for element in TAD_removed:
+                domain_removed = domain_data[1:]
+                for element in domain_removed:
                     #filter out empty space
                     if len(element) > 0:
                         genes.append(element)
@@ -53,47 +51,46 @@ for file in files:
                     end = pre_start[1]
                     #Append gene data to list of genes
                     gene_list.append([chr, start, end, gene])
-                #Create TAD dictionary entry with genes in its scope
-                TADs_with_genes[TAD] = gene_list
+                #Create domain dictionary entry with genes in its scope
+                domains_with_genes[domain] = gene_list
     printed = False
-    path = "data/mm10_data/DHS_" + file + "_cell_with_TAD.csv"
-    with open(path) as DHS_TADs:
+    path = "data/mm10_data/DHS_" + file + "_cell_with_domain.csv"
+    with open(path) as DHS_domains:
         #split file into lines
-        lis = [line.split() for line in DHS_TADs] 
+        lis = [line.split() for line in DHS_domains] 
         for bulk in lis:
-            TAD_list = []
+            dhs_list = []
             DHS_id = None
-            #Scope of the TAD data from the dhs data
+            #Scope of the domain data from the dhs data
             for DHS_data in bulk:
-                TADs = []
-                DHS_data = DHS_data.split(',')
-                DHS_id = str(DHS_data[0]) + ":" + str(DHS_data[1]) + "-" + str(DHS_data[2])
-                DHS_removed = DHS_data[4:]
-                for element in DHS_removed:
+                DHS = []
+                data = DHS_data.split(',')
+                domain_id = data[0]
+                DHS_data = DHS_data[1:]
+                for element in DHS_data:
                     if len(element) > 0:
-                        TADs.append(element)
-            for tad in TADs:
-                pre_chr = tad.split(':')
+                        DHSs.append(element)
+            for dhs in DHSs:
+                pre_chr = domain.split(':')
                 chr = pre_chr[0]
                 pre_loc = pre_chr[1]
                 pre_start = pre_loc.split('-')
                 start = pre_start[0]
                 end = pre_start[1]
-                TAD_list.append([chr, start, end, tad])
-            unique_TAD = [list(x) for x in set(tuple(x) for x in TAD_list)]
-            DHSs_with_TADs[DHS_id] = unique_TAD
+                dhs_list.append([chr, start, end, dhs])
+            DHSs_with_domains[domain_id] = domain_list
 
     printed = False
-    #Find the genes that the DHS site may interact with in the TAD
-    for dhs in DHSs_with_TADs.keys():
-        genes_in_tad = []
+    #Find the genes that the DHS site may interact with in the domain
+    for dhs in DHSs_with_domains.keys():
+        genes_in_domain = []
         distances = []
         on_left = []
         on_right = []
-        for tad in DHSs_with_TADs.values():
-            for gene_matrix in TADs_with_genes.values():
+        for domain in DHSs_with_domains.values():
+            for gene_matrix in domains_with_genes.values():
                 for gene in gene_matrix:
-                    genes_in_tad.append(gene)
+                    genes_in_domain.append(gene)
         pre_chr = dhs.split(':')
         pre_loc = pre_chr[1]
         pre_start = pre_loc.split('-')
@@ -101,7 +98,7 @@ for file in files:
         end = int(pre_start[1])
         #Find the center of the DHS site
         dhs_center = math.floor(( int(start) + int(end) ) / 2)
-        for gene in genes_in_tad:
+        for gene in genes_in_domain:
             if int(gene[2]) < dhs_center:
                 on_left.append(gene)
             elif dhs_center < int(gene[1]):
@@ -117,7 +114,7 @@ for file in files:
 
     #Export data
     Distance_df = pd.DataFrame(DHS_and_distance)
-    path = "data/mm10_data/DHS_" + file + "_nearest_full_gene_distance.csv"
+    path = "data/mm10_data/nearest_gene_in_domain/DHS_" + file + "_nearest_gene_in_domain_distance.csv"
     Distance_df.to_csv(path, index=False, header=False)
     time_to_complete = time.process_time() - last_time
     print("Time taken to process nearest genes for " + file + " cell: " + str(time_to_complete))
