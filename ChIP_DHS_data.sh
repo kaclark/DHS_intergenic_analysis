f2e10538933875adbe120f6e1ded98583c987b6b
Keanu Clark <35940194+kaclark@users.noreply.github.com>
2020-07-20 18:10:01 -0500

    Statistical Tests for ChIP data at DHS sites

--- ChIP_DHS_data.sh
+++ ChIP_DHS_data.sh
@@ -9,6 +9,6 @@ Sox2=data/ChIP_data/TF_2/macs2/Sox2_2_summits.bed
 outd=data/ChIP_data/DHS_data  
 bedtools window -w 2500 -a $DHS -b $H3K27ac > ${outd}/H3K27ac_window.bed  
 bedtools window -w 2500 -a $DHS -b $H3K4me3 > ${outd}/H3K4me3_window.bed
-bedtools intersect -a $DHS -b $Nanog > $outd/Nanog_intersect.bed
-bedtools intersect -a $DHS -b $Oct4 > $outd/Oct4_intersect.bed
-bedtools intersect -a $DHS -b $Sox2 > $outd/Sox2_intersect.bed
+bedtools intersect -wa $DHS -wb $Nanog > $outd/Nanog_intersect.bed
+bedtools intersect -wa $DHS -wb $Oct4 > $outd/Oct4_intersect.bed
+bedtools intersect -wa $DHS -wb $Sox2 > $outd/Sox2_intersect.bed
--- ChIP_Kruskal.py
+++ ChIP_Kruskal.py
@@ -1,7 +1,61 @@
 import pickle
 import pandas as pd
+from scipy import stats
 
+#load DHSs by group from pickle file
+#dictionary group[group_id] = DHSs_list
 with open("./data/groups.pickle", "rb") as pickle_in:
     groups = pickle.load(pickle_in)
 
-print(groups["in_1_only"][0])
+#ChIP_data_antigens
+ChIP_data = ["H3K27ac", "H3K4me3", "Nanog", "Sox2", "Oct4"]
+#For each antigen
+for antibody in ChIP_data:
+    direct = "./data/mm10_data/ChIP/"
+    import_path = direct + antibody + "_counts.csv"
+    data = pd.read_csv(import_path, header=None, index_col=False)
+    DHS_ids_antibody = []
+    #comes from antibody data
+    counts = []
+
+    if antibody == "Nanog":
+        print(antibody)
+
+    #list to be passed to stat test
+    group_data = []
+    #get data from dataframe
+    for row in data[0]:
+        DHS_ids_antibody.append(row)
+    for row in data[1]:
+        counts.append(row)
+
+    for key in groups.keys():
+        group_counts = []
+        group_DHSs = groups[key]
+
+        for dhs in group_DHSs:
+            if dhs in DHS_ids_antibody:
+                index_val = DHS_ids_antibody.index(dhs)
+                group_counts.append(counts[index_val])
+            else:
+                group_counts.append(0)
+        group_data.append(group_counts)
+    #print(group_data)
+    #print(antibody)
+    #print(group_data)
+    print("Kruskal test results for " + antibody)
+    print(stats.kruskal(
+        group_data[0],
+        group_data[1],
+        group_data[2],
+        group_data[3],
+        group_data[4],
+        group_data[5],
+        group_data[6],
+        group_data[7],
+        group_data[8],
+        group_data[9],
+        group_data[10],
+        group_data[11],
+        group_data[12]
+    ))
